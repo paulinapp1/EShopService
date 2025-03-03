38f01c8ca2eb93b1aeeb9a56a5bbc5112198d5af
Paulina Grabska <paulinagrabskainf@gmail.com>
2025-03-03 21:56:54 +0100

    Add project files.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace EShopService.Models
+{
+    public class Product: BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Ean { get; set; } = string.Empty;
+        public int Stock { get; set; } = 0;
+        public string sku { get; set; } = string.Empty;
+        public Category Category { get; set; } = default!;
+       
+
+        
+
+    }
+}
